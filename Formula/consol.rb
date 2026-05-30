df3d920a20d25dc85550b27701028a0d19e0856f
luwei <1280380446@qq.com>
2026-05-31 00:30:52 +0800

    Add consol formula

---
+++ Formula/consol.rb
@@ -0,0 +1,19 @@
+class Consol < Formula
+  desc "Smart contract console for Solidity and EVM development"
+  homepage "https://github.com/luweiCN/consol"
+  url "https://github.com/luweiCN/consol/archive/refs/tags/v0.1.0.tar.gz"
+  sha256 "e823837ae844246b123e76bcd56beaada8f89a3a6aff0cf086bbf665c4904637"
+  license "MIT"
+
+  depends_on "rust" => :build
+  depends_on "foundry"
+
+  def install
+    system "cargo", "install", *std_cargo_args(path: "apps/cli")
+  end
+
+  test do
+    assert_match "consol #{version}", shell_output("#{bin}/consol --version")
+    assert_match "ConSol", shell_output("#{bin}/consol --help")
+  end
+end
---
+++ README.md
@@ -0,0 +1,13 @@
+# Homebrew Tap for ConSol
+
+ConSol is the smart contract console for Solidity and EVM development.
+
+```bash
+brew tap luweiCN/consol
+brew install consol
+consol --help
+```
+
+This tap maps to `github.com/luweiCN/homebrew-consol`.
+
+The main project lives at `github.com/luweiCN/consol`.
