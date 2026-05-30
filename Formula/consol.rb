68d5f2f500a5ed8620f46548300f5f4aadb9bcd8
luwei <1280380446@qq.com>
2026-05-31 03:18:26 +0800

    consol 0.4.0

--- Formula/consol.rb
+++ Formula/consol.rb
@@ -1,8 +1,8 @@
 class Consol < Formula
   desc "Smart contract console for Solidity and EVM development"
   homepage "https://github.com/luweiCN/consol"
-  url "https://github.com/luweiCN/consol/archive/refs/tags/v0.3.0.tar.gz"
-  sha256 "8e6561dedbe70b1ccbd3727602a286c02f65caaebc016a74c6df56af61a9ed68"
+  url "https://github.com/luweiCN/consol/archive/refs/tags/v0.4.0.tar.gz"
+  sha256 "6339389bab21324696fe7721ba0a74ce579dc8ffef30308c6462f36cbfd7ebd0"
   license "MIT"
 
   depends_on "rust" => :build
